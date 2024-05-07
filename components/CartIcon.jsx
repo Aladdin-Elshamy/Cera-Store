4912625ec07c89adf27acba494fca6485bfd1c1a
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-05-07 16:01:00 +0300

    upload

--- components/CartIcon.jsx
+++ components/CartIcon.jsx
@@ -6,7 +6,7 @@ export default function CartIcon(props) {
   const { cartProducts } = props;
   return (
     <div className="cart-product-icon">
-      <Link to="/cart" className="cart-icon-container" state={cartProducts}>
+      <Link to="/cart" className="cart-icon-container">
         <FaShoppingCart className="cart-icon" />
         <span className="cart-count">{cartProducts.length}</span>
       </Link>
